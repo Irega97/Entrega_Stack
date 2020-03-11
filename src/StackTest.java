a7ecc8b5f6ab9c66a8d508458278377f91415d0e
vcarl <vcarlos139@gmail.com>
2020-03-12 00:27:10 +0100

    test con fallos

---
+++ src/StackTest.java
@@ -0,0 +1,34 @@
+public class StackTest {
+    private StackImplementation<Integer> pilaLimite;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        pilaLimite = new StackImplementation<>(5);
+        pilaLimite.push(1);
+        pilaLimite.push(2);
+    }
+    @Test(expected = PilaLlenaException.class)
+    public void testPlena() throws Exception
+    {
+        pilaLimite.push(3);
+        pilaLimite.push(4);
+        pilaLimite.push(5);
+        pilaLimite.push(3);
+    }
+    @Test(expected=PilaVaciaException.class)
+    public void testBuida() throws Exception
+    {
+        pilaLimite.pop();
+        pilaLimite.pop();
+        pilaLimite.pop();
+        assertEquals(0, pilaLimite.size());
+    }
+
+
+    @After
+    public void setDown() throws Exception
+    {
+        pilaLimite = null;
+    }
+}
